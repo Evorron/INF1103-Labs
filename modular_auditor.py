9bf1a5831b4db405ddffaf3bc0ac3a9bd5928ad6
Jerron <2605733@sit.singaporetech.edu.sg>
2026-09-19 14:59:36 +0800

    Created copy of auditor.py

---
+++ modular_auditor.py
@@ -0,0 +1,23 @@
+inventory = 0
+failed_rejected = 0
+
+while True:
+    count = input("Enter stock quantity: ")
+    # Exit condition
+    if count == "quit":
+        print("Total Units Processed:", inventory)
+        print("Number of Failed/Rejected Entries: ", failed_rejected)
+        break
+
+    elif count.isdigit() != True or inventory < 0 or count == "":
+            failed_rejected += 1
+            print("Error")
+            continue
+
+    else:
+        inventory += int(count)
+        print(inventory)
+
+        if inventory > 500:
+            print("Overstock alert")
+            break
